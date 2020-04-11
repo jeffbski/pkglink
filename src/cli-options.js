7dcc3ad2de1a79c392e9f3602ca482248d0f26ed
Jeff Barczewski <jeff.barczewski@gmail.com>
2020-04-11 12:45:13 -0500

    update @hapi/joi

--- src/cli-options.js
+++ src/cli-options.js
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
-import Joi from 'joi';
+import Joi from '@hapi/joi';
 import minimist from 'minimist';
 import OS from 'os';
 import Path from 'path';
@@ -19,40 +19,40 @@ const minimistOpts = {
     p: 'prune',
     r: 'refs-file',
     t: 'tree-depth',
-    v: 'verbose'
-  }
+    v: 'verbose',
+  },
 };
 
 export const argvSchema = Joi.object({
   config: Joi.string(),
   'refs-file': Joi.string(),
-  'tree-depth': Joi.number().integer().min(0)
-}).unknown();
+  'tree-depth': Joi.number().integer().min(0),
+}).unknown(true);
 
 export const configSchema = Joi.object({
-  refsFile: Joi.string().default(
-    Path.resolve(OS.homedir(), DEFAULT_REFS_FILE)),
+  refsFile: Joi.string().default(Path.resolve(OS.homedir(), DEFAULT_REFS_FILE)),
   concurrentOps: Joi.number().integer().min(1).default(4),
   // windows does not maintain original modtimes for installs
   // so ignoreModTime is defaulted to true for win32
-  ignoreModTime: Joi.boolean()
-                    .default(OS.platform() === 'win32'),
+  ignoreModTime: Joi.boolean().default(OS.platform() === 'win32'),
   memory: Joi.number().integer().min(100).default(2560), // MB
   minFileSize: Joi.number().integer().min(0).default(0), // bytes
   treeDepth: Joi.number().integer().min(0).default(0),
   refSize: Joi.number().integer().min(1).default(5),
-  consoleWidth: Joi.number().integer().min(30).default(70)
+  consoleWidth: Joi.number().integer().min(30).default(70),
 });
 
 export function gatherOptions(processArgv, displayHelp) {
   // processArgv is already sliced, process.argv.slice(2)
   const unvalidArgv = minimist(processArgv, minimistOpts);
-  const argvVResult = Joi.validate(unvalidArgv, argvSchema);
+  const argvVResult = argvSchema.validate(unvalidArgv);
   if (argvVResult.error) {
-    if (displayHelp) { displayHelp(); }
+    if (displayHelp) {
+      displayHelp();
+    }
     console.error('');
     console.error(chalk.red('error: invalid argument specified'));
-    argvVResult.error.details.forEach(err => {
+    argvVResult.error.details.forEach((err) => {
       console.error(err.message);
     });
     process.exit(20);
@@ -62,26 +62,28 @@ export function gatherOptions(processArgv, displayHelp) {
 }
 
 export function gatherConfig(argv, unvalidatedConfig, configPath) {
-  const configResult = Joi.validate(unvalidatedConfig, configSchema, { abortEarly: false });
+  const configResult = configSchema.validate(unvalidatedConfig, { abortEarly: false });
   if (configResult.error) {
     console.error(chalk.red('error: invalid JSON configuration'));
     console.error(`${chalk.bold('config file:')} ${configPath}`);
-    configResult.error.details.forEach(err => {
+    configResult.error.details.forEach((err) => {
       console.error(err.message);
     });
     process.exit(22);
   }
   const config = configResult.value; // with defaults applied
-  R.toPairs({ // for these defined argv values override config
+  R.toPairs({
+    // for these defined argv values override config
     dryrun: argv.dryrun,
     genLnCmds: argv['gen-ln-cmds'],
     memory: argv.memory,
     refsFile: argv['refs-file'],
-    treeDepth: argv['tree-depth']
-  }).forEach(p => {
+    treeDepth: argv['tree-depth'],
+  }).forEach((p) => {
     const k = p[0];
     const v = p[1];
-    if (!R.isNil(v)) { // if defined, use it
+    if (!R.isNil(v)) {
+      // if defined, use it
       config[k] = v;
     }
   });
@@ -93,8 +95,7 @@ export function gatherConfig(argv, unvalidatedConfig, configPath) {
 export function gatherOptionsConfig(processArgv, displayHelp) {
   const argv = gatherOptions(processArgv, displayHelp);
 
-  const CONFIG_PATH = argv.config ||
-                      Path.resolve(OS.homedir(), DEFAULT_CONFIG_FILE);
+  const CONFIG_PATH = argv.config || Path.resolve(OS.homedir(), DEFAULT_CONFIG_FILE);
 
   const parsedConfigJson = safeJsonReadSync(CONFIG_PATH);
   if (parsedConfigJson instanceof Error) {
@@ -109,7 +110,6 @@ export function gatherOptionsConfig(processArgv, displayHelp) {
 
   return {
     argv,
-    config
+    config,
   };
-
 }
